6990411603201f8ee92e8c489f448f257645c734
Ayaanuddin <khajaayaan2912@gmail.com>
2024-07-15 18:44:52 +0530

    user

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from 'mongoose'
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unqiue:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+},{timestamps:true})
+
+
+const User = mongoose.model('User',userSchema);
+export default User
